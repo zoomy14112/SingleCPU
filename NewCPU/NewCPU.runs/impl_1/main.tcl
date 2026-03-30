9a4c86672f576dc8064239e9392b750ab2d45c46
zoomy14112 <zoomy1412@outlook.com>
2026-03-30 16:30:06 +0800

    I don't know how to fix it...

--- NewCPU/NewCPU.runs/impl_1/main.tcl
+++ NewCPU/NewCPU.runs/impl_1/main.tcl
@@ -106,7 +106,6 @@ set rc [catch {
   create_msg_db init_design.pb
   set_param chipscope.maxJobs 4
   set_param general.usePosixSpawnForFork 1
-  set_param xicom.use_bs_reader 1
   set_param runs.launchOptions { -jobs 8  }
 OPTRACE "create in-memory project" START { }
   create_project -in_memory -part xc7a100tcsg324-1
